70ca60e3ef4c7ed467ebef6f6670a0144ce04f9c
Sergio Botter <sergio.botter@gmail.com>
2023-11-16 16:37:01 -0800

    First commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElasticsearchLogging.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+
+        try
+        {
+            throw new Exception("error in the application");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Something went wrong", 5);
+        }
+
+        _logger.LogInformation("Hello From Action");
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
